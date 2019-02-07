505035458328602815e705916530d4aa50c14b0a
caio <caiow2001@gmail.com>
2019-02-06 21:38:29 -0300

    Models criado e navbar do site pronta

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.categoria
+
+
+class Tipos(models.Model):
+    tipo = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.tipo
+
+
+class Habilidades(models.Model):
+    habilidade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.habilidade
+
+
+class Sexos(models.Model):
+    sexo = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.sexo
+
+
+class Pokemons(models.Model):
+    nome = models.CharField(max_length=15)
+    vida = models.IntegerField(default=0)
+    ataque = models.IntegerField(default=0)
+    defesa = models.IntegerField(default=0)
+    ataqueSp = models.IntegerField(default=0)
+    defesaSp = models.IntegerField(default=0)
+    velocidade = models.IntegerField(default=0)
+    id_evolucao = models.ForeignKey('Pokemons', on_delete=models.CASCADE, blank=True, null=True)
+    descrição = models.CharField(max_length=100, default='')
+    altura = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    peso = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default=None)
+    tipos = models.ManyToManyField(Tipos, related_name='tipos')
+    fraquezas = models.ManyToManyField(Tipos, related_name='fraquezas')
+    sexo = models.ManyToManyField(Sexos)
+    habilidades = models.ManyToManyField(Habilidades)
+
+    def __str__(self):
+        return nome
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
+from .models import Pokemons
 # Create your views here.
+class IndexView(ListView):
+    model = Pokemons
+    context_object_name = 'pokemons'
+    queryset = Pokemons.objects.all()
+    template_name = 'pokedex/index.html'
