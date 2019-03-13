518276aef3e64be990f5fb5bf06ad137b43558b8
Caio Wanderley <caiow2001@gmail.com>
2019-03-13 19:43:51 -0300

    pagiation
    pagination iniciado

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-
+from django.core.paginator import Paginator
 from .models import Pokemons
 # Create your views here.
 class IndexView(ListView):
     model = Pokemons
     context_object_name = 'pokemons'
     template_name = 'pokedex/index.html'
+
+    paginate_by = 20
+    
